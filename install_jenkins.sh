2b064ce88c9cd36cec84a16aefc182ebf5a100c5
Payton Young <43461194+payton-young@users.noreply.github.com>
2021-04-19 09:32:03 -0400

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install -y python3 python3-pip default-jdk
 sudo pip3 install ansible
 sudo pip3 install openshift
-anisible-galaxy install geerlingguy.jenkins
+ansible-galaxy install geerlingguy.jenkins
 
 # This is very bad. Do not do this in production. 
 sudo chmod 777 /var/run/docker.sock
