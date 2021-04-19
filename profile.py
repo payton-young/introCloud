d1bd4ae4d09416a6ad06472b4889c52d5577e002
Payton Young <43461194+payton-young@users.noreply.github.com>
2021-04-19 08:39:07 -0400

    Update profile.py
    copied Dr.Ngo's file that copies new scripts that were written and installs them on the VMs based on their role.

--- profile.py
+++ profile.py
@@ -1,35 +1,50 @@
 import geni.portal as portal
 import geni.rspec.pg as rspec
+import geni.rspec.igext as IG
 
 #Create a Request object to start building the RSpec.
-request = portal.context.makeRequestRSpec()
+pc = portal.Context()
+request = pc.makeRequestRSpec()
 #Create a XenVM
 
-node = request.XenVM("node")
-kubenode = request.XenVM("kubenode")
-ldapnode = request.XenVM("ldapnode")
-sqlsnode = request.XenVM("sqlsnode")
+tourDescription = \
+"""
+This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
+"""
 
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-node.routable_control_ip = "true"
+#
+# Setup the Tour info with the above description and instructions.
+#  
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
 
-kubenode.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-kubenode.routable_control_ip = "true"
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
 
-ldapnode.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-ldapnode.routable_control_ip = "true"
+num_nodes = 3
+for i in range(num_nodes):
+  if i == 0:
+    node = request.XenVM("head")
+  else:
+    node = request.XenVM("worker-" + str(i))
+  node.cores = 4
+  node.ram = 8192
+  node.routable_control_ip = "true" 
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
 
-sqlsnode.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-sqlsnode.routable_control_ip = "true"
+  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_kubernetes.sh"))
+  node.addService(pg.Execute(shell="sh", command="sudo swapoff -a"))
+  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
 
-
-
-node.addService(rspec.Execute(shell="/bin/sh",
-                                    command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh",
-                                    command="sudo apt install mysql-server"))
-node.addService(rspec.Execute(shell="/bin/sh",
-                                     command='sudo mysql_secure_installation -y'))
-
-#Print the RSpec to the enclosing page.
-portal.context.printRequestRSpec()
+  if i == 0:
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_jenkins.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_manager.sh"))
+  else:
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_worker.sh"))
+    
+pc.printRequestRSpec(request)
